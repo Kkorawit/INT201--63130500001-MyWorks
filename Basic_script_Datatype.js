c07bb3bba3cc03c4f302ffa6e6f52c47ad23a30e
Kkorawit <korawit.sit@mail.kmutt.ac.th>
2021-08-17 13:09:54 +0700

    First Script

---
+++ Basic_script_Datatype.js
@@ -0,0 +1,51 @@
+// Permitive type and Value
+// let var const
+// function , operator
+// Synchonous , Asychonous
+
+
+// let A = 'name';
+// // let A = 'name2';//error
+// let B = 'L-name';
+// let C = '631305xxxx';
+
+// let D = `${A} ${B} ${C}`;
+// console.log(D);
+
+// var G = 'name';
+// var G = 'name L-name';//not error
+// console.log(G);
+
+
+// const L = 'Dname';
+// const L = 'Fname';//error
+
+// console.log(moo);
+// // console.log(baz);// ReferenceError
+// {
+//     var moo = "Mooo"
+//     let baz = "Bazz";
+//     console.log(moo, baz); // Mooo Bazz
+//   }
+
+//   console.log(moo); // Mooo
+// //   console.log(baz); // ReferenceError
+
+let kkk = 'kkk';
+var pp = 'pp';
+{
+    console.log(kkk);
+    console.log(pp);
+    let batt = 'batt';
+    var fii = 'fii';
+}
+console.log(kkk);
+console.log(pp);
+console.log(batt);
+console.log(fii);
+
+
+
+
+
+
